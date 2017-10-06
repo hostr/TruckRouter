840ad85def6ab762262b418677584b2fe0d7cce6
Ryan Host <ryanhostrh@gmail.com>
2017-10-06 14:19:13 -0500

    Add solving service template

---
+++ MapSolver/Services/SolvingService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MapSolver.Interfaces;
+
+namespace MapSolver.Services
+{
+    public class SolvingService : ISolvingService
+    {
+        public void Solve(string[] maze)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
